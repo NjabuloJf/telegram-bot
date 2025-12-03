bf0df9f03374f01e7de8b73a8ad3be00abe0cdc5
Njabulo Jb <fanajb65@gmail.com>
2025-12-03 20:37:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 global.prefix = '/'; // Set Your Preferred Prefix
-global.botName = 'GIFTED-MD'; // Bot Name
+global.botName = 'Njabulo-Jb'; // Bot Name
 global.botVersion = '2.0.2';
 global.botPic = 'https://files.giftedtech.web.id/file/gifted-md.jpg'; // Can use path, can also use link
 global.botToken = '7267479440:AAFUOGy7mqGNCI8klIkOqb0QzYoYCQ9o8Rw'; // Replace With Your Bot Token (Get it from BotFather)
 global.ownerUsername = 'mauricegift'; // Replace With Your Telegram Username(For Bot Owner)
 global.timeZone = 'Africa/Nairobi'; // Your Time Zone
 global.ownerId = [8084255827]; // Replace With Your Owner Chat ID(For Bot Owner), You Can Add More than 1
-global.footer = '*Powered by GiftedTech*';
+global.footer = '*Powered by Njabulo Jb*';
 global.ytdlWeb = 'https://youtube.giftedtech.web.id';
 global.giftedYtdlpApi = 'https://ytapi.giftedtech.co.ke';
 global.giftedApi = 'https://api.giftedtech.co.ke/api'; // Gifted Api
 global.giftedKey = 'gifted'; // Gifted Api Key
 global.lyffeApi = 'https://levanter.onrender.com'; // Lyffe Api
 global.giftedWaChannel = 'https://whatsapp.com/channel/0029Vb3hlgX5kg7G0nFggl0Y';
-global.giftedRepo = "https://github.com/mauricegift/telegram-bot";
+global.giftedRepo = "https://github.com/NjabuloJf/telegram-bot";
 global.giftedApiRepo = "https://api.github.com/repos/mauricegift/telegram-bot";
 
 global.giftechMess = {
